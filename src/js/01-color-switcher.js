b005bfad034ec243ec78183eedd9be3e613596d8
etelight <etelight@gmail.com>
2023-06-26 16:26:41 +0300

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtnEl = document.querySelector('button[data-start]');
+const stopBtnEl = document.querySelector('button[data-stop]');
+let timerChangeId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+stopBtnEl.disabled = true;
+
+startBtnEl.addEventListener('click', e => {
+  e.preventDefault();
+  stopBtnEl.disabled = false;
+  startBtnEl.disabled = true;
+  timerChangeId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopBtnEl.addEventListener('click', e => {
+  e.preventDefault();
+  stopBtnEl.disabled = true;
+  startBtnEl.disabled = false;
+  clearInterval(timerChangeId);
+});
