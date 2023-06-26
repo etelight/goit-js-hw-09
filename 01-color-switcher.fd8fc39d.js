3425358546c271c1963b0b46c4dff96ccc86d7cb
etelight <105778055+etelight@users.noreply.github.com>
2023-06-26 13:27:32 +0000

    Deploying to gh-pages from @ etelight/goit-js-hw-09@b005bfad034ec243ec78183eedd9be3e613596d8 🚀

---
+++ 01-color-switcher.fd8fc39d.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let a=null;e.disabled=!0,t.addEventListener("click",(d=>{d.preventDefault(),e.disabled=!1,t.disabled=!0,a=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(d=>{d.preventDefault(),e.disabled=!0,t.disabled=!1,clearInterval(a)}));
+//# sourceMappingURL=01-color-switcher.fd8fc39d.js.map
